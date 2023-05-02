6d6c688acf68c006887e63a5ea3ce90e4c009921
zєєт χєη <nexenband@gmail.com>
2023-05-02 13:14:03 +0530

    first commit

---
+++ wave.js
@@ -0,0 +1,18 @@
+const labels= document.querySelectorAll('.form-control label');
+// console.log(labels);
+
+labels.forEach(label =>{
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx)=> `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('')
+});
+
+
+// const labels = document.querySelectorAll('.form-control label');
+
+// labels.forEach(label =>{
+//     label.innerHTML = label.innerText.split('').map((letter,index)=>`<span style="transition-delay:${index * 50}ms">${letter}</span>`).join('')
+    
+// });
+
